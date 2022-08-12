5b28c56b6f4605b411849924aab4c5c83d5081bb
moaath-sh <moaathsh16@gmail.com>
2022-08-13 00:56:41 +0300

    cv_task

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var scalImg =document.getElementById("scal-img");
+
+scalImg.onmouseover=function(){
+    scalImg.className="img-scale";
+}
+scalImg.onmouseleave=function(){
+    scalImg.className="img-norm";
+}
